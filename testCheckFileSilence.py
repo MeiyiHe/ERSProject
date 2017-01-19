90a910b6bbad3c5f19a0ab6954f44797b4405718
Meiyi <hemeiyi0225@gmail.com>
2017-01-19 13:22:16 -0800

    add comments

--- testCheckFileSilence.py
+++ testCheckFileSilence.py
@@ -18,28 +18,41 @@ chunks = split_on_silence(sound,
     # consider it silent if quieter than -16 dBFS
     silence_thresh=-40
 )
+
+# store the splitted voice files into chunks and store it into a[]
 a = []
 for i, chunk in enumerate(chunks):
     chunk.export("./splittedAudioFiles/chunk{0}.wav".format(i), format="wav")
+
 for i, chunk in enumerate(chunks):
 	a.append((os.path.getsize("./splittedAudioFiles/chunk{0}.wav".format(i)),i))
 
+
+# check how many elements in the array a 
 bfLen = len(a)
+
 
 print "before sorted "
 print a 
 
+
+# sorted_a[] will store the chunk in an ascending file size order
 sorted_a = sorted(a, key = lambda x:x[0])
 print "after sorted "
 print sorted_a
 
 
+# remove the empty file from sorted[] by check the difference between expected word numbers 
+# and the actual word numbers
 indexToRemove = []
 for j in range(0,5):
 	indexToRemove.append(sorted_a[j][1])
 	print sorted_a[j][1]
 
 	os.remove("./splittedAudioFiles/chunk{0}.wav".format(indexToRemove[j]))
+
+
+
 
 
 
