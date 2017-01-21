131ded04b0329674aa70dc0825ac6d7d75048a42
Meiyi <hemeiyi0225@gmail.com>
2017-01-21 11:36:23 -0800

    redirect google output to new text file

---
+++ googleScript.py
@@ -0,0 +1,34 @@
+from sys import argv
+import speech_recognition as sr
+import os
+
+#usage: python googleScript.py testWrite.txt
+
+script, filename = argv
+
+print "We're going to write on%r." % filename
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+
+unrelated_paths = ['..', './splittedAudioFiles']
+chunk_files = (os.path.join(p, o) for p in unrelated_paths
+              for o in os.listdir(p)
+              if (o.lower().endswith('.wav')
+                  and os.path.isfile(os.path.join(p, o))))
+# use the audio file as the audio source
+
+r = sr.Recognizer()
+for chunk_file in chunk_files:
+    print chunk_file
+    with sr.AudioFile(chunk_file) as source:
+    	audio = r.record(source) # read the entire audio file
+	try:
+		#print("Google thinks you said " + r.recognize_google(audio))
+		target.write(r.recognize_google(audio))
+		target.write("\n")
+	except sr.UnknownValueError:
+		target.write("Google could not understand audio")
+	except sr.RequestError as e:
+	    target.write("Google error; {0}".format(e))
---
+++ testWrite.py
@@ -0,0 +1,3 @@
+user input 1
+user input 2
+user input 3
