822bb89177ed164ef97234d636018981e4075e0c
lilusha <lul004@ucsd.edu>
2017-01-26 14:34:40 -0800

    finish testAligner

--- testAligner.py
+++ testAligner.py
@@ -1,23 +1,44 @@
 from pydub import AudioSegment
 from difflib import SequenceMatcher
+import speech_recognition as sr
+from os import path
 
 # Get the voice inputs
-var raw_input = ("Please enter the scripts to be processed: ")
+var = raw_input("Please enter the scripts to be processed: ")
 
 words = var.split()
 
+combined_sounds = AudioSegment.from_wav(words[0]+".wav")
+
 # Concatenate the voice inputs
-for word in words
-	sound1 = AudioSegment.from_wav(word+".wav")
-	sound2 = AudioSegment.from_wav(word+".wav")
-	combined_sounds = combined_sound + sound1 + sound2
+for i in range(len(words)):
+	if i==0 :
+		continue
+	combined_sounds=combined_sounds+AudioSegment.from_wav(words[i]+".wav")
 
 # Generate the voice outputs
 combined_sounds.export("Output.wav", format="wav")
 
+# obtain path to "english.wav" in the same folder as this script
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "Output.wav")
+#AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
+#AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
+
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source) # read the entire audio file
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    combined = r.recognize_google(audio)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 # Output the accuracy
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-
-print similar(var, )
+print SequenceMatcher(None, var, combined).ratio()
