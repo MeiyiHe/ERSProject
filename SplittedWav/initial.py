394018fb9c9076347370e0f550c8868fd615a2c9
lilusha <lul004@ucsd.edu>
2017-02-09 15:38:08 -0800

    try

--- SplittedWav/initial.py
+++ SplittedWav/initial.py
@@ -1,34 +1,9 @@
-from pydub import AudioSegment
-from difflib import SequenceMatcher
 import speech_recognition as sr
 from os import path
 import os
 
-# Get the voice inputs
-var = raw_input("Please enter the scripts to be processed: ")
-
-input_file = open(var,'rb')
-inputs = input_file.read()
-words = inputs.split()
-
-combined_sounds = AudioSegment.from_file(words[0]+".wav")
-combined_sounds = combined_sounds.set_channels(2)
-#print combined_sounds.channels
-
-
-# Concatenate the voice inputs
-for i in range(len(words)):
-	if i==0 :
-		continue
-	new_sound_file = AudioSegment.from_file(words[i] + ".wav")
-	new_sound_file = new_sound_file.set_channels(2)
-	combined_sounds=combined_sounds+new_sound_file
-
-# Generate the voice outputs
-combined_sounds.export("Output.wav", format="wav")
-
-# obtain path to "english.wav" in the same folder as this script
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "Output.wav")
+#obtain path to "english.wav" in the same folder as this script
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "A.wav")
 #AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
 #AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
 
@@ -50,5 +25,3 @@ except sr.RequestError as e:
 
 print inputs
 print combined
-# Output the accuracy
-print SequenceMatcher(None, inputs, combined).ratio()
