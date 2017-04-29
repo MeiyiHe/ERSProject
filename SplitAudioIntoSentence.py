5fcd579b3b2a51c9f8b6916b464436ce4d3bd36a
Meiyi He <hemeiyi0225@gmail.com>
2017-04-28 17:33:05 -0700

    add setcovering & uniquelist

--- SplitAudioIntoSentence.py
+++ SplitAudioIntoSentence.py
@@ -7,16 +7,22 @@ import os
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-sound = AudioSegment.from_mp3("./TestAudio2.MP3")
+#average_loudness = podcast.rms
+
+
+sound = AudioSegment.from_mp3("/Users/meiyihe/Desktop/testSOS/170203_004.MP3")
+average_loudness = sound.rms
+#silence_threshold = average_loudness * db_to_float(-30)
+
 chunks = split_on_silence(sound, 
     # must be silent for at least half a second(>=1200)
-    min_silence_len=1200,
+    min_silence_len=275,
 
     # consider it silent if quieter than -40 dBFS
     silence_thresh=-40
 )
 for i, chunk in enumerate(chunks):
-    out_file = "./chunk{0}.wav".format(i)
+    out_file = "/Users/meiyihe/Desktop/testSOS/outChunks4/chunk{0}.wav".format(i)
     print "exporting", out_file
     chunk.export(out_file, format="wav")
 
