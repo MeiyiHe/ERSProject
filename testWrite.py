89c91f06ce5b4b830177d82d1d19dc282257f728
Hi <sil142@ucsd.edu>
2017-05-20 11:09:22 -0700

    reordering

--- testWrite.py
+++
@@ -1,3 +0,0 @@
-user input 1
-user input 2
-user input 3
--- textSplit.py
+++
@@ -1,9 +0,0 @@
-
-
-
-with open("testTxt1.txt") as f:
-	lines = f.readlines()
-  	for line in lines:
-  		words = line.split()
-  		for word in words:
-  			print (word)
